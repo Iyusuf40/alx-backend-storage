5ba05bbd55826668a56d13684372b8d90f8bbf8b
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-11 22:18:33 +0000

    add create users table script

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 -- duplicate comment enforced
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE,
-    name VARCHAR(255) UNIQUE
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 )
