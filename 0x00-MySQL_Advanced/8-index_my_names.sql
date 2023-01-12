ceed34c16727c8c132d74888075668c1f450a5ae
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-12 13:31:53 +0000

    create index

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,6 @@
+-- script creates an index idx_name_first on the table names and the first letter of name
+
+-- DROP INDEX idx_name_first ON names;
+
+CREATE INDEX idx_name_first
+ON names (name (1));
