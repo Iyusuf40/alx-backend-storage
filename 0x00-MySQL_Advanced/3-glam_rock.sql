0d2067231633f2bfafceeef5fcad88888e01b351
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-12 00:22:35 +0000

    use function to check if split year is NULL

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,30 @@
 -- script lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, (split - formed) as lifespan
+DROP FUNCTION IF EXISTS nulToCurrentYear;
+
+DELIMITER //
+
+-- function checks if date is null and returns current year instead
+CREATE FUNCTION nulToCurrentYear(date1 year) RETURNS year DETERMINISTIC
+BEGIN
+    DECLARE date2 year;
+    SET date2 = year(curdate());
+    IF date1 IS NULL
+    THEN
+        RETURN date2;
+    ELSE
+        RETURN date1;
+    END IF;
+END 
+
+//
+
+DELIMITER ;
+
+SELECT band_name, (nulToCurrentYear(split) - formed) as lifespan
 FROM
     metal_bands
-WHERE style = "Glam rock"
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
+
+DROP FUNCTION nulToCurrentYear;
