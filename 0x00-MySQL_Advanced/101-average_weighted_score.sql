f1f40847f3ea4f8aceefa6312c40dd41421036f9
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-12 17:50:12 +0000

    use subquery to get all weighted avgs grouped by user_ids

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,25 +1,44 @@
 -- script creates a stored procedure ComputeAverageWeightedScoreForUsers 
 -- that computes and store the weighted average score for a student.
 
+-- STRATEGY: GET ALL WEIGHTED AVG OF ALL USERS BY USING A SUBQUERY / WITH CTE
+-- UPDATE ALL USERS BASED ON THE ABOVE EXTRACTED TABLE
+
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers ;
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
-    -- CREATE TEMP TABLE TO HOLD EACH USER WITH THEIR AGGREGATED WEIGHTED AVG;
-    WITH 
-    av AS ( 
-        SELECT corrections.user_id as id, SUM( corrections.score * projects.weight) / SUM(projects.weight) 
-        AS scores
-            FROM  corrections
-            JOIN projects
-            ON projects.id = corrections.project_id
-            GROUP BY 1
-    )
-    -- USE BOTH TEM TABLE 'av' AND USERS TABLE TO UPDATE CALCULATED AVG
-    UPDATE users, av SET users.average_score = av.scores
-    WHERE av.id = users.id;
+    UPDATE users, (
+        -- SUBQUERY TO GET ALL WEIGHTED AVG OF ALL USERS 
+        SELECT corrections.user_id as id, SUM( corrections.score * projects.weight) / SUM(projects.weight) as scores
+        FROM  corrections
+        JOIN projects
+        ON projects.id = corrections.project_id
+        GROUP BY 1
+    ) as av SET average_score = av.scores WHERE av.id = users.id;
 END//
 
 DELIMITER ;
+
+-- WITH CTE VERSION BELOW
+-- DELIMITER //
+
+-- DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers ;
+
+-- CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+-- BEGIN
+--     WITH 
+--     av AS ( 
+--         SELECT corrections.user_id as id, SUM( corrections.score * projects.weight) / SUM(projects.weight) as scores
+--             FROM  corrections
+--             JOIN projects
+--             ON projects.id = corrections.project_id
+--             GROUP BY 1
+--     )
+--     UPDATE users, av SET users.average_score = av.scores
+--     WHERE av.id = users.id;
+-- END//
+
+-- DELIMITER ;
