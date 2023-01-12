57ac805c791387acfdabd76fd39c2feccb9f9c40
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-12 08:19:14 +0000

    use coalesce function

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,31 +1,7 @@
 -- script lists all bands with Glam rock as their main style, ranked by their longevity
-
-DROP FUNCTION IF EXISTS nulToCurrentYear;
-
-DELIMITER //
-
--- function checks if date is null and returns current year instead
-CREATE FUNCTION nulToCurrentYear(date1 YEAR) RETURNS YEAR DETERMINISTIC
-BEGIN
-    DECLARE date2 YEAR;
-    SET date2 = 2020; -- YEAR(CURDATE());
-    IF date1 IS NULL
-    THEN
-        RETURN date2;
-    ELSE
-        RETURN date1;
-    END IF;
-END 
-
-//
--- return to default delimeter
-DELIMITER ;
-
 -- main select statement
-SELECT band_name, (nulToCurrentYear(split) - formed) as lifespan
+SELECT band_name, COALESCE((split - formed), (YEAR(CURDATE()) - formed)) as lifespan
 FROM
     metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC, 1 ASC;
-
-DROP FUNCTION nulToCurrentYear;
