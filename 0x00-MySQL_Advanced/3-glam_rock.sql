99f87d53505a67b51d334d6e9f195128f8e4e409
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-12 08:23:08 +0000

    use coalesce function and current year

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- script lists all bands with Glam rock as their main style, ranked by their longevity
 -- main select statement
-SELECT band_name, COALESCE((split - formed), (2020 - formed)) as lifespan
+SELECT band_name, COALESCE((split - formed), (YEAR(CURDATE()) - formed)) as lifespan
 FROM
     metal_bands
 WHERE style LIKE "%Glam rock%"
