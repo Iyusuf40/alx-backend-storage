e12a8cfffb37fb59889cc073268eb6cddb592b89
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-17 19:38:27 +0000

    reduce no of print

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,37 +9,29 @@ def main():
     """ main functuon """
     client = MongoClient('mongodb://127.0.0.1:27017')
     col = client.logs.nginx
-    nginx_collection = client.logs.nginx
-    # docs_count = col.count_documents({})
-    # get_count = col.count_documents({"method": "GET"})
-    # post_count = col.count_documents({"method": "POST"})
-    # put_count = col.count_documents({"method": "PUT"})
-    # patch_count = col.count_documents({"method": "PATCH"})
-    # del_count = col.count_documents({"method": "DELETE"})
-    # status_check = col.count_documents({"method": "GET", "path": "/status"})
-    # print("{} logs".format(docs_count))
-    # print("Methods:")
-    # print("    method GET: {}".format(get_count))
-    # print("    method POST: {}".format(post_count))
-    # print("    method PUT: {}".format(put_count))
-    # print("    method PATCH: {}".format(patch_count))
-    # print("    method DELETE: {}".format(del_count))
-    # print("{} status check".format(status_check))
+    docs_count = col.count_documents({})
+    get_count = col.count_documents({"method": "GET"})
+    post_count = col.count_documents({"method": "POST"})
+    put_count = col.count_documents({"method": "PUT"})
+    patch_count = col.count_documents({"method": "PATCH"})
+    del_count = col.count_documents({"method": "DELETE"})
+    status_check = col.count_documents({"method": "GET", "path": "/status"})
+    print("{} logs".format(docs_count))
+    print("Methods:")
 
-    n_logs = nginx_collection.count_documents({})
-    print(f'{n_logs} logs')
+    lst = [
+        ("GET", get_count),
+        ("POST", post_count),
+        ("PUT", put_count),
+        ("PATCH", patch_count),
+        ("DELETE", del_count)
+    ]
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+    for method, count in lst:
+        print(f"    method {method}: {count}")
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
+    print("{} status check".format(status_check))
 
-    print(f'{status_check} status check')
     client.close()
 
 
