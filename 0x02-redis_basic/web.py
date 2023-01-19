cd9b86e9e0ece8fc055eae0737be5e68380c3917
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-19 10:00:35 +0000

    use decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,27 +5,43 @@
 import redis
 import requests
 import time
+import functools
+from typing import Callable
 
 
+r = redis.Redis()
+
+
+def dec(fn: Callable) -> Callable:
+    """ decorator implements caching """
+
+    @functools.wraps(fn)
+    def wr(url):
+        """ wrapper func """
+        key = "count:" + url
+        content_key = "cached:" + url
+        r.incr(key)
+        cached_page = r.get(content_key)
+        if cached_page:
+            return str(cached_page.decode("utf-8"))
+
+        return fn(url)
+
+    return wr
+
+
+@dec
 def get_page(url: str) -> str:
-    """ tracks no of times a url is visited """
-    r = redis.Redis()
-    key = "count:" + url
-    content_key = "cont:" + url
-    cached_page = r.get(content_key)
-    r.incr(key)
-    if cached_page:
-        return cached_page.decode("utf-8")
-    else:
-        res = requests.get(url)
-        r.setex(content_key, 10, res.text)
+    """ returns a page at url"""
+    res = requests.get(url)
+    content_key = "cached:" + url
+    r.setex(content_key, 10, res.text)
     return res.text
 
 
 if __name__ == "__main__":
     url = "https://flexrecords.cloza.tech/"
     key = "count:" + url
-    r = redis.Redis()
     for i in range(10):
         res = get_page(url)
         print(r.get(key))
