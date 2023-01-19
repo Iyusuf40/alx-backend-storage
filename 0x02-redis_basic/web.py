9bc5952e4a1b303079d55381c2677bb8a5498e22
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-01-19 10:31:58 +0000

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,57 +1,37 @@
 #!/usr/bin/env python3
-""" mod's doc string """
+""" Module for Implementing an expiring web cache and tracker """
 
-
+from functools import wraps
 import redis
 import requests
-import time
-import functools
 from typing import Callable
-
 
 r = redis.Redis()
 
 
-def dec(method: Callable) -> Callable:
-    """ decorator implements caching """
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting how many times a request
+    has been made """
 
-    @functools.wraps(method)
-    def wr(url):
-        """ wrapper func """
-        key = "count:" + url
-        content_key = "cached:" + url
-        r.incr(key)
-        cached_page = r.get(content_key)
-        if cached_page:
-            return str(cached_page.decode("utf-8"))
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
-        res = method(url)
-        r.setex(content_key, 10, res)
-        return res
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
 
-    return wr
+    return wrapper
 
 
-@dec
+@count_requests
 def get_page(url: str) -> str:
-    """ returns a page at url"""
-    res = requests.get(url)
-    return res.text
-
-
-if __name__ == "__main__":
-    url = "https://flexrecords.cloza.tech/"
-    key = "count:" + url
-    for i in range(10):
-        res = get_page(url)
-        print(r.get(key))
-        print(res[:20])
-    time.sleep(5)
-    res = get_page(url)
-    print("===== after 5 secs =====")
-    print(res[:20])
-    time.sleep(5)
-    res = get_page(url)
-    print("===== after 10 secs =====")
-    print(res[:20])
-    print(r.get(key))
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
+    """
+    req = requests.get(url)
+    return req.text
